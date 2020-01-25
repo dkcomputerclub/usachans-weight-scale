5b987cb489cfa1d2a0d90ddc6f5985f951bed12e
江田裕貴 <yuki.eda.010@gmail.com>
2020-01-25 16:16:50 +0900

    EDAフォルダのスタイリングコードを整理

--- EDA/App.js
+++ EDA/App.js
@@ -9,26 +9,25 @@ import { StyleSheet,
 import WeightAim from './src/weightAim';
 
 
-
 export default class App extends React.Component {
   render(){
     let user = {weightNow:45, aimweight:42};
 
   return (
     <View style={[styles.container]}>
-      <View style={[styles.base, styles.header]}>
-        <Text style={[styles.text, styles.textBig, styles.textRed, styles.textBold]}>Usakoのきろく</Text>
+      <View style={[styles.header]}>
+        <Text style={[styles.textHeader]}>Usakoのきろく</Text>
       </View>
 
-      <View style={[styles.base, styles.today]}>
-        <Text style={[styles.text, styles.textBig]}>12月7日</Text>
-        <Text style={[styles.text, styles.textMedium]}>13:05</Text>
+      <View style={[styles.today]}>
+        <Text style={[styles.textDate]}>12月7日</Text>
+        <Text style={[styles.textTime]}>13:05</Text>
       </View>
 
       <View style={[styles.base, styles.weight_today]}>
-        <Text style={[styles.text, styles.textPink, styles.textBold,]}>今日の体重は</Text>
+        <Text style={[styles.text, styles.textTodaysWeight]}>今日の体重は</Text>
         <View style={styles.boxA}>
-          <Text style={[styles.text, styles.textWhite, styles.textBig, styles.textBold,]}>{user.weightNow} kg</Text>
+          <Text style={[styles.textWeightNow]}>{user.weightNow} kg</Text>
         </View>
       </View>
       <WeightAim aim={user.aimweight}/>
@@ -38,48 +37,45 @@ export default class App extends React.Component {
 }
 
 const styles = StyleSheet.create({
+  //for container--------------------------
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'white',
     // alignItems: 'center',
     justifyContent: 'center',
   },
 
-  // for text-------------------------------------------
+  //for texts------------------------------
+
   text: {
     fontSize: 24,
     color: 'black',
   },
 
-  textBig: {
+  textHeader: {
     fontSize: 38,
-  },
-
-  textMedium: {
-    fontSize: 27,
-  },
-
-  textSmall: {
-    fontSize: 12,
-  },
-
-  textWhite: {
-    color: 'white',
-  },
-
-  textRed: {
     color: 'red',
+    fontWeight: 'bold',
   },
 
-  textPink: {
+  textDate: {
+    fontSize: 38,
+    color: 'black',
+  },
+
+  textTime: {
+    fontSize: 28,
+  },
+
+  textWeightNow: {
+    fontSize: 38,
+    color: 'white',
+    fontWeight: 'bold',
+  },
+
+  textTodaysWeight: {
+    fontSize: 28,
     color: '#ff99cc',
-  },
-
-  textHotpink: {
-    color: 'hotpink',
-  },
-
-  textBold: {
     fontWeight: 'bold',
   },
 
@@ -87,6 +83,9 @@ const styles = StyleSheet.create({
   base: {
     justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: 'grey',
+//    borderColor: 'grey',
+//    borderWidth: 1,
   },
 
   boxA: {
@@ -96,6 +95,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#ff99cc',
     alignItems: 'center',
     justifyContent: 'center',
+
   },
 
   boxB: {
@@ -110,16 +110,22 @@ const styles = StyleSheet.create({
   },
 
   header: {
+    justifyContent: 'center',
+    alignItems: 'center',
     flex: 3,
     backgroundColor: 'white',
   },
 
   today: {
+    justifyContent: 'center',
+    alignItems: 'center',
     flex: 2,
     backgroundColor: 'white',
   },
 
   weight_today:{
+    justifyContent: 'center',
+    alignItems: 'center',
     flex: 4,
     backgroundColor: 'white',
   },
--- GCP/react-native/App.js
+++ GCP/react-native/App.js
@@ -83,7 +83,7 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#d2ffeb',
     // alignItems: 'center',
     justifyContent: 'center',
   },
